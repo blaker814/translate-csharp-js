3e4263d592e7c22f77f0b80a02adaa99aa7f6660
blaker814 <rab0196@yahoo.com>
2020-11-13 14:01:30 -0600

    translated grimiore to js

--- grimoire/js/grimiore/main.js
+++ grimoire/js/grimiore/main.js
@@ -1,6 +1,70 @@
 function main() {
   // Put your code here
+  console.log("Do you believe in magic?");
+  console.log("------------------------");
 
+  const allSpells = GetAllSpells();
+  const goodBook = MakeGoodSpellBook(allSpells);
+  const evilBook = MakeEvilSpellBook(allSpells);
+
+  DisplaySpellBook(goodBook);
+  console.log("");
+  DisplaySpellBook(evilBook);
+}
+
+function DisplaySpellBook(book) {
+  console.log(book.Title);
+  for (let aSpell of book.Spells) {
+    console.log(aSpell.Name);
+  }
+}
+
+function MakeEvilSpellBook(allSpells) {
+  const evilBook = {};
+  evilBook.Title = "Evil Book";
+  evilBook.Spells = allSpells.filter(spell => spell.IsEvil)
+
+  return evilBook;
+}
+
+function MakeGoodSpellBook(allSpells) {
+  const goodBook = {};
+  goodBook.Title = "Good Book";
+  goodBook.Spells = allSpells.filter(spell => !spell.IsEvil);
+
+  return goodBook;
+}
+
+function GetAllSpells() {
+  const allSpells = [
+    {
+      Name: "Turn enemy into a newt",
+      IsEvil: true,
+      EnergyReqired: 5.1
+    },
+    {
+      Name: "Conjure some gold for a local charity",
+      IsEvil: false,
+      EnergyReqired: 2.99
+    },
+    {
+      Name: "Give a deaf person the ability to heal",
+      IsEvil: false,
+      EnergyReqired: 12.2
+    },
+    {
+      Name: "Make yourself emperor of the universe",
+      IsEvil: true,
+      EnergyReqired: 100.0
+    },
+    {
+      Name: "Convince your relatives your political views are correct",
+      IsEvil: false,
+      EnergyReqired: 2921.5
+    }
+  ];
+
+  return allSpells;
 }
 
 main();
